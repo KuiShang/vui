df9878d14bfb981d728c9171e97a3519714ee4c4
kk <shangkuikui@gomeplus.com>
2018-01-18 16:59:21 +0800

    bug fix vui ->vu

--- packages/icon-button/src/icon-button.vue
+++ packages/icon-button/src/icon-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <i class="vui-icon" :class="`vui-icon-${name}`" v-on="$listeners">
+  <i class="vu-icon" :class="`vu-icon-${name}`" v-on="$listeners">
     <slot></slot>
     <div v-if="info" class="icon__info">{{ info }}</div>
   </i>
